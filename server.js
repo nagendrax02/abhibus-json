46577647b588108223911c1827aa775ecae03cc3
nagendrax02 <rawatnagendra98@gmail.com>
2021-12-14 18:24:08 +0530

    commit to server js

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 server.use(middlewares)
 server.use(router)
-server.listen(port, () => {
-  console.log('JSON Server is running')
-})
+server.listen(process.env.PORT || 8000);
